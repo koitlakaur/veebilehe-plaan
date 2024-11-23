7c701fdf73f737f23c793c59628e91da24f5db76
koitlakaur <koitla.kaur@gmail.com>
2024-11-23 12:08:41 +0200

    commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Burger menüü kuvamise ja peitmise funktsioon
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+}
