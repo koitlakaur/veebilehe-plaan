a901a4c3e543894b0b831fbdc946e0278379b4e4
koitlakaur <koitla.kaur@gmail.com>
2024-11-23 12:26:38 +0200

    commit

--- script.js
+++
@@ -1,6 +0,0 @@
-// Funktsioon menüü kuvamiseks ja peitmiseks
-function toggleMenu() {
-    const menu = document.getElementById('menu');
-    // Lisab või eemaldab 'show' klassi, mis muudab menüü nähtavaks
-    menu.classList.toggle('show');
-}
