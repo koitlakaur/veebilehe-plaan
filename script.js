3089996e14a353e1aa243c730d50f26987d5d5f0
koitlakaur <koitla.kaur@gmail.com>
2024-11-23 12:12:33 +0200

    commit

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-// Burger menüü kuvamise ja peitmise funktsioon
-function toggleMenu() {
-    const menu = document.getElementById('menu');
-    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
-}
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
